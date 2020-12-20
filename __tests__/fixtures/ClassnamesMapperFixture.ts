1aff42b2faefb5503f2913136c519817ed0e6ee8
streletss <xstreletssx@gmail.com>
2020-12-21 01:44:30 +0200

    fix ClassnamesMapper type

---
+++ __tests__/fixtures/ClassnamesMapperFixture.ts
@@ -0,0 +1,5 @@
+import ClassnamesMapper from "../../src/3_adapters/ClassnamesMapper";
+
+const ClassnamesMapperFixture = ClassnamesMapper.make();
+
+export default ClassnamesMapperFixture;
--- src/3_adapters/ClassnamesMapper/ClassnamesMapper.ts
+++ src/3_adapters/ClassnamesMapper/ClassnamesMapper.ts
@@ -1,9 +1,14 @@
+interface _ProxyConstructor {
+  new <T, H extends object>(target: T, handler: ProxyHandler<H>): H;
+}
+const _Proxy = Proxy as _ProxyConstructor;
+
 export default (p: { valueSymbol: string }) => {
-  return function makeClassnamesMapper(map: Record<string, string>) {
-    return new Proxy(map, {
-      get(target, name: string) {
-        return (val: string) => {
-          const mapValue = name in target ? target[name] : `${name}_${val}`;
+  return function makeClassnamesMapper(map: Record<string, string> = {}) {
+    return new _Proxy<typeof map, Record<string, (v?: string) => string>>(map, {
+      get(_, name: string) {
+        return (val: string = "") => {
+          const mapValue = name in map ? map[name] : `${name}_${val}`;
           return mapValue.replace(p.valueSymbol, val);
         };
       },

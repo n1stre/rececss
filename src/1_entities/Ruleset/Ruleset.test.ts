4236d0164b8e79168384b2f1da87c45a5a9f331d
streletss <xstreletssx@gmail.com>
2021-02-11 18:40:19 +0200

    add basic domain tests

--- src/1_entities/Ruleset/Ruleset.test.ts
+++ src/1_entities/Ruleset/Ruleset.test.ts
@@ -2,9 +2,18 @@ import { IRulesetDTO } from "./Ruleset.interface";
 import Ruleset from "./index";
 
 const defaultRuleset: IRulesetDTO = {
-  // id: "fakeRuleset",
+  classname: "myClass",
+  declarations: "top: 0; bottom: 0;",
 };
 
 describe("Ruleset entity", () => {
-  it("", () => {});
+  it("should be presented as a string", () => {
+    const rs = Ruleset.make(defaultRuleset);
+    expect(rs.toString()).toBe(".myClass { top: 0; bottom: 0; }");
+  });
+
+  it("should be renamed properly", () => {
+    const rs = Ruleset.make(defaultRuleset).rename((old) => "prefix__" + old);
+    expect(rs.toString()).toBe(".prefix__myClass { top: 0; bottom: 0; }");
+  });
 });
--- src/2_usecases/GenerateUtilityRulesets/GenerateUtilityRulesets.test.ts
+++
@@ -1,9 +0,0 @@
-import GenerateUtilityRulesets from "./index";
-
-const usecase = GenerateUtilityRulesets.build();
-
-describe("GenerateUtilityRulesets usecase", () => {
-  it("should", async () => {
-    //
-  });
-});

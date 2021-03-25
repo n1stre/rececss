0facf4bc161679a480b310af927fd37a7d3bc854
streletss <xstreletssx@gmail.com>
2021-03-25 18:03:30 +0300

    update ruleset spec

--- src/1_entities/Ruleset/Ruleset.test.ts
+++ src/1_entities/Ruleset/Ruleset.test.ts
@@ -1,9 +1,8 @@
-import { IRulesetDTO } from "./Ruleset.interface";
 import Ruleset from "./index";
 
 describe("Ruleset entity", () => {
   it("should be presented as a string", () => {
-    const rs = Ruleset.make({
+    const rs = Ruleset.create({
       classname: "myClass",
       declarations: "top: 0; bottom: 0;",
     });
@@ -12,7 +11,7 @@ describe("Ruleset entity", () => {
   });
 
   it("should escape classname", () => {
-    const rs = Ruleset.make({
+    const rs = Ruleset.create({
       classname: "md:w_20.5%",
       declarations: "width: 20.5%;",
     });
@@ -20,12 +19,24 @@ describe("Ruleset entity", () => {
     expect(rs.toString()).toBe(".md\\:w_20\\.5\\% { width: 20.5%; }");
   });
 
-  it("should be renamed properly", () => {
-    const rs = Ruleset.make({
-      classname: "myClass",
-      declarations: "top: 0; bottom: 0;",
-    }).rename((old) => "prefix__" + old);
+  it("should attach pseudo classes", () => {
+    const rs = Ruleset.create({
+      classname: "t_0",
+      declarations: "top: 0;",
+      pseudoClasses: { h: "$0:hover", ja: "$0.js-active" },
+    });
 
-    expect(rs.toString()).toBe(".prefix__myClass { top: 0; bottom: 0; }");
+    expect(rs.toString()).toBe(
+      ".t_0, .t_0\\:h:hover, .t_0\\:ja.js-active { top: 0; }",
+    );
+  });
+
+  it("should add prefix to classname", () => {
+    const rs = Ruleset.build({ prefixSep: "__" }).create({
+      classname: "t_0",
+      declarations: "top: 0;",
+    });
+    rs.addClassnamePrefix("prefix");
+    expect(rs.toString()).toBe(".prefix__t_0 { top: 0; }");
   });
 });

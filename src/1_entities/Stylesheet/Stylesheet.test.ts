682be14074d544108f44826f85b7030474f98fb8
streletss <xstreletssx@gmail.com>
2021-03-26 18:05:43 +0300

    update stylesheet spec

--- src/1_entities/Stylesheet/Stylesheet.test.ts
+++ src/1_entities/Stylesheet/Stylesheet.test.ts
@@ -1,24 +1,23 @@
-import { IStylesheetDTO } from "./Stylesheet.interface";
 import Stylesheet from "./index";
 
 const defaultRulesets = [
-  { classname: "m_a", declarations: "margin: auto;" },
-  { classname: "fullwidth", declarations: "width: 100%;" },
+  ".m_a { margin: auto; }",
+  ".fullwidth { width: 100%; }",
 ];
 
 describe("Stylesheet entity", () => {
   it("should have valid contents", () => {
-    const style = Stylesheet.make({ rulesets: defaultRulesets });
-    expect(style.getContents()).toBe(
+    const style = Stylesheet.create({ rulesets: defaultRulesets });
+    expect(style.toString()).toBe(
       ".m_a { margin: auto; }\n.fullwidth { width: 100%; }\n",
     );
   });
 
   it("should wrap contents in media query", () => {
     const media = { name: "max-md", query: "screen and (max-width: 700px)" };
-    const style = Stylesheet.make({ rulesets: defaultRulesets, media });
-    expect(style.getContents()).toBe(
-      "@media screen and (max-width: 700px) {\n.max-md\\:m_a { margin: auto; }\n.max-md\\:fullwidth { width: 100%; }\n}\n",
+    const style = Stylesheet.create({ rulesets: defaultRulesets, media });
+    expect(style.toString()).toBe(
+      "@media screen and (max-width: 700px) {\n.m_a { margin: auto; }\n.fullwidth { width: 100%; }\n}\n",
     );
   });
 });

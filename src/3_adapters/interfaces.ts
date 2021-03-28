3e97447d7a7f40786e544a51b5656efbb3c8b0c4
streletss <xstreletssx@gmail.com>
2021-03-28 18:37:53 +0300

    update IO interface

--- src/3_adapters/interfaces.ts
+++ src/3_adapters/interfaces.ts
@@ -1,6 +1,19 @@
 import { TConfigurableRulesetsValues } from "../2_usecases/interfaces";
+import { IRulesetNamesMap } from "./RulesetsBuilder/RulesetsBuilder.interface";
 
 export interface IInputOutput {
+  getRulesetPropsInput(): {
+    prefixSep?: string;
+    suffixSep?: string;
+  };
+  getStylesheetPropsInput(): {
+    filename?: string;
+    extension?: string;
+  };
+  getRulesetsBuilderInput(): {
+    classNames?: Partial<IRulesetNamesMap>;
+    pseudoClasses?: Record<string, string>;
+  };
   getStylesheetsAssetsInput(): {
     values: TConfigurableRulesetsValues;
     media?: Record<string, string>;

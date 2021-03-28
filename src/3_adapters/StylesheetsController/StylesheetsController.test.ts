79086fad224ed12365cb1c0dd9da7ca9777329cd
streletss <xstreletssx@gmail.com>
2021-03-28 18:40:32 +0300

    pass entities props to GenerateStylesheetAssets

--- src/3_adapters/StylesheetsController/StylesheetsController.test.ts
+++ src/3_adapters/StylesheetsController/StylesheetsController.test.ts
@@ -1,15 +1,22 @@
 import { IInputOutput } from "../interfaces";
 import StylesheetsController from "./index";
 
+const io: IInputOutput = {
+  getRulesetPropsInput: jest.fn(() => ({})),
+  getStylesheetPropsInput: jest.fn(() => ({})),
+  getRulesetsBuilderInput: jest.fn(() => ({})),
+  getStylesheetsAssetsInput: jest.fn(() => ({ values: {}, media: {} })),
+  outputAssets: jest.fn(),
+};
+
 describe("StylesheetsController", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it("should generate assets without errors", async () => {
-    const io: IInputOutput = {
-      getStylesheetsAssetsInput: jest.fn(() => ({ values: {}, media: {} })),
-      outputAssets: jest.fn(),
-    };
     const controller = StylesheetsController.new(io);
     const res = await controller.generateAssets();
-
     expect(io.getStylesheetsAssetsInput).toBeCalledTimes(1);
     expect(io.outputAssets).toBeCalledTimes(1);
   });
--- src/3_adapters/StylesheetsController/StylesheetsController.ts
+++ src/3_adapters/StylesheetsController/StylesheetsController.ts
@@ -8,9 +8,13 @@ export default () => {
     constructor(private io: IInputOutput) {}
 
     async generateAssets() {
-      const factory = AllRulesetsFactory.new(RulesetsBuilder.new());
-      const usecase = GenerateStylesheetAssets.new(factory);
-      const assets = await usecase.exec(this.io.getStylesheetsAssetsInput());
+      const builder = RulesetsBuilder.new(this.io.getRulesetsBuilderInput());
+      const assets = await GenerateStylesheetAssets.create({
+        RulesetsFactory: AllRulesetsFactory.new(builder),
+        rulesetProps: this.io.getRulesetPropsInput(),
+        stylesheetProps: this.io.getStylesheetPropsInput(),
+      }).exec(this.io.getStylesheetsAssetsInput());
+
       await this.io.outputAssets(assets);
     }
   };

a86939793e6746dc559ee5af879ad28bc19d2b5b
streletss <xstreletssx@gmail.com>
2021-04-09 12:33:24 +0300

    fix: config rules units

--- rececss.config.js
+++ rececss.config.js
@@ -22,84 +22,73 @@ module.exports = {
   },
   rules: {
     width: {
-      named: { full: "100%", half: "50%" },
-      units: { px: [[0, 50, 5], [100, 1000, 100]], "%": [[0, 100, 10]] },
+      full: "100%", half: "50%",
+      $px: [[0, 50, 5], [100, 1000, 100]],
+      $pct: [[0, 100, 10]]
     },
     height: {
-      named: { full: "100%", half: "50%" },
-      units: { px: [300, 50], vh: [50, 100] },
+      full: "100%", half: "50%",
+      $px: [300, 50],
+      $vh: [50, 100]
     },
     padding: {
       shorthand: { a: "auto" },
       edges: {
-        named: { sm: "10px", md: "15px", lg: "30px", xl: "50px" },
-        units: { px: [[0, 50, 5]], em: [3, 1.3], "%": [] },
+        sm: "10px", md: "15px", lg: "30px", xl: "50px",
+        $px: [[0, 50, 5]],
+        $em: [3, 1.3]
       },
     },
     margin: {
       shorthand: { a: "auto", "0a": "0 auto" },
       edges: {
-        named: { sm: "10px", md: "15px", lg: "30px", xl: "50px" },
-        units: { px: [[0, 50, 5]] },
+        sm: "10px", md: "15px", lg: "30px", xl: "50px",
+        $px: [[0, 50, 5]]
       },
     },
     offset: {
-      named: { xs: "5px", sm: "10px", md: "15px", lg: "30px", xl: "50px" },
-      units: { px: [[0, 100, 5]] },
+      xs: "5px", sm: "10px", md: "15px", lg: "30px", xl: "50px",
+      $px: [[0, 100, 5]]
     },
     flex: {
       shorthand: {
-        named: { "00a": "0 0 auto", norm: "1" },
-        values: [[1, 10, 1]],
+        "00a": "0 0 auto", norm: "1",
+        $num: [[1, 10, 1]]
       },
       basis: {
-        named: { xs: "8px", sm: "12px", md: "16px" },
-        units: { px: [[8, 16, 2]], "%": [[0, 100, 20]] },
+        xs: "8px", sm: "12px", md: "16px",
+        $px: [[8, 16, 2]],
+        $pct: [[0, 100, 20]]
       },
-      grow: {
-        named: {},
-        values: [1, 2, 4],
-      },
+      grow: { $num: [1, 2, 4] },
+      shrink: { $num: [1, 3] },
+      order: { $num: [1, -1] },
       grid: {
         cols: 12,
         gutter: "20px",
         gutters: { sm: "10px", lg: "40px"}
-      },
-      shrink: {
-        named: {},
-        values: [1, 3],
-      },
-      order: {
-        named: {},
-        values: [1, -1],
       },
     },
     font: {
       shorthand: {
         primary: "italic bold .8em/1.2 Arial, sans-serif",
       },
-      size: {
-        named: { xs: "8px", sm: "12px", md: "16px" },
-        units: { px: [[8, 16, 2]] },
-      },
       family: {
         primary: "Arial, serif",
         secondary: "Helvetica, sans-serif",
       },
+      size: {
+        xs: "8px", sm: "12px", md: "16px",
+        $px: [[8, 16, 2]]
+      },
       lineHeight: {
-        named: { xs: "8px", sm: "12px", md: "16px" },
-        values: [1, 2, [3, 4, 0.2]]
+        xs: "8px", sm: "12px", md: "16px",
+        $num: [1, 2, [3, 4, 0.2]]
       }
     },
     border: {
-      shorthand: {
-        thin: "1px solid black",
-        bold: "5px solid black"
-      },
-      radius: {
-        named: { circle: "50%" },
-        units: { px: [5, 10] },
-      },
+      shorthand: { thin: "1px solid black", bold: "5px solid black" },
+      radius: { circle: "50%", $px: [5, 10] },
     },
     background: {
       shorthand: {

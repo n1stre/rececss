0f7aebbd0c3c74993120764d557529d27b9b8eec
streletss <xstreletssx@gmail.com>
2021-04-11 16:05:19 +0300

    docs: update examples

--- examples/components/rececss.config.js
+++ examples/components/rececss.config.js
@@ -1,5 +1,4 @@
-const colors = { dark: "#000", light: "#fff", red: "red" }
-const spacing = { sm: "10px", md: "15px", lg: "30px", xl: "50px" }
+// rececss.config.js
 
 module.exports = {
   output: {
@@ -8,84 +7,34 @@ module.exports = {
   },
   media: {
     md: "only screen and (min-width: 768px)",
+    lg: "only screen and (min-width: 1024px)"
   },
   states: {
     h: "$0:hover",
   },
   rules: {
-    width: {
-      named: { full: "100%", half: "50%" },
-      units: { px: [[0, 50, 5], [100, 1000, 100]], "%": [[0, 100, 10]] },
-    },
-    height: {
-      named: { full: "100%", half: "50%" },
-      units: { px: [300, 50] },
-    },
+    width: { $px: [50, 100], $pct: [100] },
+    height: { $px: [50, 100] },
     padding: {
-      shorthand: { a: "auto" },
-      edges: {
-        named: spacing,
-      },
+      edges: { $px: [[0, 50, 5]] }
     },
     margin: {
-      shorthand: { a: "auto", "0a": "0 auto" },
-      edges: {
-        named: spacing,
-      },
-    },
-    offset: {
-      named: spacing,
-      units: { px: [[0, 100, 5]] },
-    },
-    flex: {
-      shorthand: {
-        named: { "00a": "0 0 auto", norm: "1" },
-        values: [[1, 10, 1]],
-      },
-      basis: {
-        named: { xs: "8px", sm: "12px", md: "16px" },
-        units: { "%": [[0, 100, 20]] },
-      },
-      grid: {
-        cols: 12,
-        gutter: "20px",
-        gutters: { sm: "10px", lg: "40px"}
-      },
+      edges: { $px: [-80, [0, 50, 5]] }
     },
     font: {
-      shorthand: {
-        primary: "italic bold .8em/1.2 Arial, sans-serif",
-      },
-      size: {
-        named: { xs: "8px", sm: "12px", md: "16px" },
-        units: { px: [[8, 16, 2]] },
-      },
-      family: {
-        primary: "Arial, serif",
-        secondary: "Helvetica, sans-serif",
-      },
-      lineHeight: {
-        named: { xs: "8px", sm: "12px", md: "16px" },
-        values: [1, 2, [3, 4, 0.2]]
-      }
+      size: { $px: [[8, 24, 2]] },
+      family: { pri: "'Archivo Black', sans-serif", sec: "'Montserrat', sans-serif" },
     },
     border: {
-      shorthand: {
-        thin: "1px solid black",
-        bold: "5px solid black"
-      },
-      radius: {
-        named: { circle: "50%" },
-        units: { px: [5, 10] },
-      },
+      shorthand: { sep: "1px solid rgba(0,0,0,0.1)", frame: "10px solid #fff", },
+      radius: { $px: [15], $pct: [50] },
     },
     background: {
-      shorthand: {
-        grn: "green",
-        gra: "content-box radial-gradient(crimson, skyblue);"
-      },
-      color: colors
+      color: { light: "#fff", blue: "#ECF0FB", }
     },
-    color: colors,
+    shadow: {
+      box: { air: "0px 4px 12px rgba(0, 0, 0, 0.08)", }
+    },
+    color: { blue: "#51568A" },
   },
 }

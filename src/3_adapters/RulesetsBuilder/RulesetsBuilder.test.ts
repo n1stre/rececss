63798ce0e05631763749dc445ba381b9dbf0c03e
streletss <xstreletssx@gmail.com>
2021-04-19 23:54:02 +0300

    test: add missing specs for rulesets builder

--- src/3_adapters/RulesetsBuilder/RulesetsBuilder.test.ts
+++ src/3_adapters/RulesetsBuilder/RulesetsBuilder.test.ts
@@ -1,11 +1,49 @@
 import RulesetsBuilder from "./index";
 
 describe("RulesetsBuilder", () => {
-  it("should add rulesets from names", () => {});
+  it("should add rulesets from names", () => {
+    const builder = RulesetsBuilder.create({});
+    builder.addRulesetsFromNames([
+      "displayBlock",
+      "visibilityHidden",
+      "alignContentCenter",
+    ]);
 
-  it("should add rulesets from values and names", () => {});
+    expect(builder.getResultDTO()).toEqual([
+      { classname: "d_b", declarations: "display: block;" },
+      { classname: "v_h", declarations: "visibility: hidden;" },
+      { classname: "ac_c", declarations: "align-content: center;" },
+    ]);
+  });
 
-  it("should add ruleset with injecteed states", () => {});
+  it("should add rulesets from values and names", () => {
+    const builder = RulesetsBuilder.create({});
+    builder.addRulesetsFromValues({ "2": "2px", "6rem": "6rem" }, ["width"]);
 
-  it("should add ruleset from dto", () => {});
+    expect(builder.getResultDTO()).toEqual([
+      { classname: "w_2", declarations: "width: 2px;" },
+      { classname: "w_6rem", declarations: "width: 6rem;" },
+    ]);
+  });
+
+  it("should add ruleset with injecteed states", () => {
+    const classnameStates = { h: "$:hover" };
+    const builder = RulesetsBuilder.create({ classnameStates });
+    builder.addRulesetsFromValues({ "2": "2px" }, ["width"]);
+
+    expect(builder.getResultDTO()).toEqual([
+      { classname: "w_2", declarations: "width: 2px;", classnameStates },
+    ]);
+  });
+
+  it("should add ruleset from dto", () => {
+    const builder = RulesetsBuilder.create({});
+    builder.addRuleset({ classname: "w_2", declarations: "width: 2px;" });
+    builder.addRuleset({ classname: "w_6", declarations: "width: 6px;" });
+
+    expect(builder.getResultDTO()).toEqual([
+      { classname: "w_2", declarations: "width: 2px;" },
+      { classname: "w_6", declarations: "width: 6px;" },
+    ]);
+  });
 });

b4e453ab02fc917a38471dc2ecc2ec7a737682b3
streletss <xstreletssx@gmail.com>
2021-05-14 17:42:41 +0300

    feat: expose all utils

--- src/4_infrastructures/utils/index.ts
+++ src/4_infrastructures/utils/index.ts
@@ -1,2 +1,5 @@
 export { default as Range } from "./Range";
+export { default as Sizing } from "./Sizing";
+export { default as Pallete } from "./Pallete";
 export { default as CSSProcessor } from "./CSSProcessor";
+export { default as DataTypes } from "./DataTypes";

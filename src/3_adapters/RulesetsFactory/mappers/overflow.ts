cb72879fd28e424c781f19577fbeb163d38daadd
streletss <xstreletssx@gmail.com>
2021-05-25 15:02:03 +0300

    feat: map overflow values - add overflowInline and overflowBlock

--- src/3_adapters/RulesetsFactory/mappers/overflow.ts
+++ src/3_adapters/RulesetsFactory/mappers/overflow.ts
@@ -2,5 +2,11 @@ import { RulesetsBuilder } from "../RulesetsFactory.interface";
 
 export default (builder: RulesetsBuilder) =>
   function mapOverflow(values: Record<string, string>) {
-    builder.mapValuesToRulesets(values, ["overflow", "overflowX", "overflowY"]);
+    builder.mapValuesToRulesets(values, [
+      "overflow",
+      "overflowX",
+      "overflowY",
+      "overflowInline",
+      "overflowBlock",
+    ]);
   };

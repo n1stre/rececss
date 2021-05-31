6856989399ea8621699d857552b307c731b97db5
streletss <streletss@users.noreply.github.com>
2021-05-31 14:38:02 +0000

    deploy: 3adfe231bfd7024a47b78448fe7c382beea5d09d

--- _next/static/cNSsVGuqh1gMo82QD8weP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/baa773718aad3fd36464.css","static/chunks/pages/index-96da5996570567160f95.js"],"/_error":["static/chunks/pages/_error-6367991aa20d6882057e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/i3IRF1U5keYm1E9WVd_bD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/807e5e50742573cf1fb4.css","static/chunks/pages/index-ceaa900c0252d54771ef.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

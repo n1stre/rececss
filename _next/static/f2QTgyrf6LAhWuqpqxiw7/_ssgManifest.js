471fce357570a499d7629e55e4cb164f125213d5
streletss <streletss@users.noreply.github.com>
2021-08-24 13:08:48 +0000

    deploy: 7a158a476813e18f2df9b29975188fa3431569ce

--- _next/static/f2QTgyrf6LAhWuqpqxiw7/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcomponents\u002Fforms\u002FButton","\u002Fcomponents\u002Fforms\u002FTextInput","\u002Fdocs\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/hw7vfx-2n9KtXO-Nf1C61/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcomponents\u002Fforms\u002FButton","\u002Fcomponents\u002Fforms\u002FTextInput","\u002Fcomponents\u002Fforms\u002FTextarea","\u002Fdocs\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()

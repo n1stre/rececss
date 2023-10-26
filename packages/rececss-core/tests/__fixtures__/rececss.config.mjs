9d1a77966fa20c1a5858862df283ee231aa7c109
streletss <xstreletssx@gmail.com>
2023-10-26 18:05:52 +0100

    test(fixtures): add mjs config

---
+++ packages/rececss-core/tests/__fixtures__/rececss.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  output: {
+    path: "./website/styles",
+    filename: "rececss",
+    extension: "css",
+    splitByMedia: false,
+    purge: { content: ["./website/pages/**/*.js"] },
+  },
+  media: {
+    md: "only screen and (min-width: 768px)",
+    lg: "only screen and (min-width: 1024px)",
+  },
+  sep: {
+    media: ":",
+    variant: ":",
+  },
+  values: {},
+};
